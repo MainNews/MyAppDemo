3f6cd8a263beaba12dce13d17b1f44ec8cb2de18
hank <123456>
2017-04-22 00:05:42 +0800

    添加alpha(透明度),scale(缩放),translate(平称),rotate(旋转),set(动画集合),interpolator(插值器)对应代码实现的方式

--- app/src/main/java/com/example/hank/myappdemo/mveiw/animation/Alpha_Scale_Translate_Rotate_Set.java
+++ app/src/main/java/com/example/hank/myappdemo/mveiw/animation/Alpha_Scale_Translate_Rotate_Set.java
@@ -17,7 +17,14 @@ import butterknife.OnClick;
 
 /**
  * Created by Jun on 2017/4/19.
- * 该类用于展示Alpha_Scale_Translate_Rotate_Set的基础用法与效果
+ * 该类用于展示
+ *          Alpha（透明度）
+ *          Scale（缩放）
+ *          Translate（平移）
+ *          Rotate（旋转）
+ *          Set（动画集合）
+ *          Interpolator（插值器）
+ * 基础用法与效果，以及以代码方式创建的对象Class
  */
 
 public class Alpha_Scale_Translate_Rotate_Set extends BaseActivtiy {
@@ -42,26 +49,69 @@ public class Alpha_Scale_Translate_Rotate_Set extends BaseActivtiy {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.alpha, R.id.scale, R.id.rotate, R.id.translate,R.id.set})
+    @OnClick({R.id.alpha, R.id.scale, R.id.rotate, R.id.translate, R.id.set})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.alpha:
+                /*代码对应的构造函数
+                AlphaAnimation(Context context, AttributeSet attrs)  同样，从本地XML加载动画，基本不用
+                AlphaAnimation(float fromAlpha, float toAlpha)*/
                 startAnimation(R.anim.alpha_anim);
                 break;
             case R.id.scale:
+                /*代码对应的构造函数
+                ScaleAnimation(Context context, AttributeSet attrs)  从XML文件加载动画，基本用不到
+                ScaleAnimation(float fromX, float toX, float fromY, float toY)
+                ScaleAnimation(float fromX, float toX, float fromY, float toY, float pivotX,
+                float pivotY)
+                ScaleAnimation(float fromX, float toX, float fromY, float toY, int pivotXType,
+                float pivotXValue, int pivotYType, float pivotYValue)*/
                 startAnimation(R.anim.scaleanim);
                 break;
             case R.id.rotate:
+                /* 代码对应的构造函数
+                *   RotateAnimation(Context context, AttributeSet attrs)　　从本地XML文档加载动画，同样，基本不用
+                    RotateAnimation(float fromDegrees, float toDegrees)
+                    RotateAnimation(float fromDegrees, float toDegrees, float pivotX, float pivotY)
+                    RotateAnimation(float fromDegrees, float toDegrees, int pivotXType, float
+                    pivotXValue, int pivotYType, float pivotYValue)
+                * */
                 startAnimation(R.anim.rotate_anim);
                 break;
             case R.id.translate:
+                /*代码对应的构造函数
+                TranslateAnimation(Context context, AttributeSet attrs)  同样，基本不用
+                TranslateAnimation(float fromXDelta, float toXDelta, float fromYDelta, float toYDelta)
+                TranslateAnimation(int fromXType, float fromXValue, int toXType, float toXValue,
+                int fromYType,floatfromYValue, int toYType, float toYValue)
+                 */
                 startAnimation(R.anim.translate_anim);
                 break;
             case R.id.set:
+                /*代码对应的构造函数
+                setAnim = new AnimationSet(true);
+                setAnim.addAnimation(Animation a)
+                public void addAnimation (Animation a)
+                 */
                 startAnimation(R.anim.set_anim);
                 break;
         }
     }
+    /* 插值器类对比
+
+    在使用过程中，通过动画类.setInterpolator(Interpolator);
+        如 AnimationSet.setInterpolator(new AccelerateDecelerateInterpolato());
+                Interpolator class	                            Resource ID
+        AccelerateDecelerateInterpolato         @android:anim/accelerate_decelerate_interpolator
+        AccelerateInterpolator	                @android:anim/accelerate_interpolator
+        AnticipateInterpolator	                @android:anim/anticipate_interpolator
+        AnticipateOvershootInterpolator	        @android:anim/anticipate_overshoot_interpolator
+        BounceInterpolator	                    @android:anim/bounce_interpolator
+        CycleInterpolator	                    @android:anim/cycle_interpolator
+        DecelerateInterpolator	                @android:anim/decelerate_interpolator
+        LinearInterpolator	                    @android:anim/linear_interpolator
+        OvershootInterpolator	                @android:anim/overshoot_interpolator
+     */
 
     private void startAnimation(int animationLayout) {
         Animation animation = AnimationUtils.loadAnimation(this, animationLayout);
