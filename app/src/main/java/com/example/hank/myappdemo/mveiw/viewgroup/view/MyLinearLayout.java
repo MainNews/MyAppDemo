d81ff428cf22d0b4e2a8e0a13d6048c45b28d53c
hank <123456>
2017-05-19 23:08:52 +0800

    给MyLinearLayout.java添加margin属性，使在xml布局中的margin属性可以正常用的使用，主要使用到了MarginLayoutParams类，该类对margin的值进行了一个抽取(获取)

--- app/src/main/java/com/example/hank/myappdemo/mveiw/viewgroup/view/MyLinearLayout.java
+++ app/src/main/java/com/example/hank/myappdemo/mveiw/viewgroup/view/MyLinearLayout.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
  * 自定义一个与LinearLayout类似的控件
  */
 
-public class MyLinearLayout  extends ViewGroup{
+public class MyLinearLayout extends ViewGroup {
 
     public MyLinearLayout(Context context) {
         super(context);
@@ -33,13 +33,18 @@ public class MyLinearLayout  extends ViewGroup{
         int height = 0;
         int width = 0;
         int count = getChildCount();//获如子控件的个数
-        for (int i=0;i<count;i++){
+        for (int i = 0; i < count; i++) {
             //测量子控件
             View child = getChildAt(i);
-            measureChild(child,widthMeasureSpec,heightMeasureSpec);
-            //获得子控件的高与宽
-            int childWidth = child.getMeasuredWidth();
-            int childHeight = child.getMeasuredHeight();
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+
+            MarginLayoutParams marginLayoutParams = (MarginLayoutParams) child.getLayoutParams();
+
+            //获得子控件的高与宽,并加上相应的margin属性值
+            int childWidth = child.getMeasuredWidth() + marginLayoutParams.leftMargin
+                    + marginLayoutParams.rightMargin;
+            int childHeight = child.getMeasuredHeight() + marginLayoutParams.topMargin
+                    + marginLayoutParams.bottomMargin;
             /*
                 因为我们是垂直排列其内部所有的VIEW，
                       所以container所占宽度应该是各个TextVIew中的最大宽度，
@@ -47,7 +52,7 @@ public class MyLinearLayout  extends ViewGroup{
              */
             //得到最大的高宽进行累加，这样就知道ViewGroup需要多少空间
             height += childHeight;
-            width = Math.max(childWidth,width);
+            width = Math.max(childWidth, width);
         }
         /*
             wrap_content-> MeasureSpec.AT_MOST
@@ -71,22 +76,56 @@ public class MyLinearLayout  extends ViewGroup{
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int top = 0;
         int count = getChildCount();
-        for (int i=0;i<count;i++){
+        for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
-            int childWidth = child.getMeasuredWidth();
-            int childHeight = child.getMeasuredHeight();
 
-            child.layout(0,top,childWidth,top + childHeight);
+            MarginLayoutParams marginLayoutParams = (MarginLayoutParams) child.getLayoutParams();
+
+
+            int childWidth = child.getMeasuredWidth() + marginLayoutParams.leftMargin
+                    + marginLayoutParams.rightMargin;
+            int childHeight = child.getMeasuredHeight() + marginLayoutParams.topMargin
+                    + marginLayoutParams.bottomMargin;
+
+            child.layout(0, top, childWidth, top + childHeight);
             top += childHeight;
 
         }
-
-
-
-
     }
+
     @Override//将控件绘制出来
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+    }
+
+    /*
+       重写generateLayoutParams()方法，获取Xml中设置的margin属性
+
+     */
+
+    /**
+     * 从指定的XML中获取对应的layout_width和layout_height值，默认情况下只会获取高宽的值
+     */
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
+    }
+
+
+    /**
+     * 为了得到指定的XML中的属性值，所以这里要重写generateLayoutParams()方法，来得到设定的属性，
+     *      而这里只是在例子中得到的margin的属性值
+     */
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    /**
+    * 如果要使用默认的构造方法，就生成layout_width="wrap_content"、layout_height="wrap_content"对应的参数
+    */
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
     }
 }
