328742b64af6905f529e46dd3b8bc49aec2fb7b1
hank <123456>
2017-06-03 21:05:09 +0800

    添加手势绘图

--- app/src/main/java/com/example/hank/myappdemo/mveiw/draw/activity/DrawBezierActivity.java
+++ app/src/main/java/com/example/hank/myappdemo/mveiw/draw/activity/DrawBezierActivity.java
@@ -3,21 +3,31 @@ package com.example.hank.myappdemo.mveiw.draw.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
+import android.view.View;
 
 import com.example.hank.myappdemo.R;
 import com.example.hank.myappdemo.mveiw.draw.view.MyViewDrawBezier;
+
 /**
  * Created by Jun on 2017/5/31.
  * 该界面用于展示绘制的贝赛尔曲线
  */
 
 public class DrawBezierActivity extends AppCompatActivity {
+
+    private MyViewDrawBezier myViewDrawBezier;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_view_draw_noe_view_layout);
-        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.draw_noe_view_layout);
-        frameLayout.addView(new MyViewDrawBezier(DrawBezierActivity.this));
+        setContentView(R.layout.my_view_draw_activity_bezier_layout);
+
+        myViewDrawBezier = (MyViewDrawBezier) findViewById(R.id.view_draw_bezier_my_draw_bezier);
+        findViewById(R.id.view_draw_bezier_but_reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myViewDrawBezier.reset();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/hank/myappdemo/mveiw/draw/view/MyViewDrawBezier.java
+++ app/src/main/java/com/example/hank/myappdemo/mveiw/draw/view/MyViewDrawBezier.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -14,12 +15,27 @@ import android.view.View;
  */
 
 public class MyViewDrawBezier extends View {
+    private Path mPath = new Path();
     public MyViewDrawBezier(Context context) {
         super(context);
     }
 
     public MyViewDrawBezier(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(event.getX(),event.getY());
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(event.getX(),event.getY());
+                postInvalidate();//重绘，该方法可以不在Ui线程中使用
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
@@ -37,8 +53,17 @@ public class MyViewDrawBezier extends View {
         path.quadTo(400,400,500,300);//连着调用，将以上面的终点为起点
 
         canvas.drawPath(path,paint);
+        /*
+            实现手指轨迹
+         */
 
-
-
+        canvas.drawPath(mPath,paint);
+    }
+    /**
+        该方法给外部调用，对界面进行重新绘制
+     */
+    public void reset(){
+        mPath.reset();
+        invalidate();//重绘，但该方法必须在UI线程中使用
     }
 }
